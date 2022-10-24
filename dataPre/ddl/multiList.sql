951063e88301d164bfd67b03058cec0d2f29e693
linrol <linrolmail@gmail.com>
2022-10-24 12:32:10 +0800

    branch-task-0000-update ddl

--- dataPre/ddl/multiList.sql
+++ dataPre/ddl/multiList.sql
@@ -326,8 +326,6 @@ CREATE TABLE "baseapp_query_list_definition"
     "group_id" VARCHAR(64),
     "object_type" VARCHAR(64),
     "device_type_id" VARCHAR(64) DEFAULT 'DeviceType.pc',
-    "is_my_bill" BOOLEAN DEFAULT false NOT NULL,
-    "is_reference" BOOLEAN DEFAULT false NOT NULL,
     "is_approval" BOOLEAN DEFAULT false NOT NULL,
     "is_exhibition" BOOLEAN DEFAULT false NOT NULL,
     "is_my_bill" BOOLEAN DEFAULT false NOT NULL,
