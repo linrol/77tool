1f4aab016aa6aae78c435875f76280129dc9d049
linrol <linrolmail@gmail.com>
2022-10-30 16:11:26 +0800

    branch-task-0000-code optimization

---
+++ api/common.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import redis
+import subprocess
+from request import post_form
+from log import logger
+sys.path.append("/Users/linrol/work/sourcecode/qiqi/backend/branch-manage")
+sys.path.append("/root/data/sourcecode/qiqi/backend/branch-manage")
+sys.path.append("/data/backend/branch-manage")
+from branch import utils
+
+
+class Common:
+    def __init__(self):
+        self.utils = utils
+        self.projects = utils.project_path()
+        self.project_build = self.projects.get('build')
+        self.project_init_data = self.projects.get('init-data')
+        self.redis_pool = redis.ConnectionPool(host="linrol.cn", port=6379,
+                                               password='linrol_redis', db=2,
+                                               decode_responses=True,
+                                               max_connections=16)
+
+    def exec(self, command, throw=False):
+        [ret, msg] = subprocess.getstatusoutput(command)
+        logger.info("exec[{}] ret[{}]".format(command, msg))
+        if throw and ret != 0:
+            raise Exception(str(msg))
+        return [ret == 0, msg]
+
+    def chdir_branch(self):
+        os.chdir("../branch/")
+
+    def chdir_data_pre(self):
+        os.chdir("../dataPre/")
+
+    # 是否首次创建分支
+    def init_create_branch(self, branch):
+        return self.project_build.getBranch(branch) is None
+
+    # 获取清空build脚本的参数
+    def get_clear_build_params(self, branch):
+        if self.init_create_branch(branch):
+            return "true"
+        else:
+            return "false"
+
+    # 切换本地分支
+    def checkout_branch(self, branch_name):
+        cmd = 'cd ../branch;python3 checkout.py {}'.format(branch_name)
+        return self.exec(cmd, True)
+
+    # 删除分支
+    def clear_branch(self, branch_name):
+        try:
+            cmd = 'cd ../branch;python3 checkanddeleted.py {} none'.format(branch_name)
+            return self.exec(cmd)
+        except Exception as err:
+            return False, str(err)
+
+    # 保护分支
+    def protect_branch(self, branch, protect):
+        try:
+            protect_cmd = "cd ../branch;python3 protectBranch.py {} {}".format(branch, protect)
+            return self.exec(protect_cmd)
+        except Exception as err:
+            return False, str(err)
+
+    # 触发ops编译
+    def ops_build(self, branch, skip=False):
+        try:
+            build_url = "http://ops.q7link.com:8000/qqdeploy/projectbuild/"
+            if skip:
+                return
+            post_form(build_url, {"branch": branch, "byCaller": "值班助手"})
+        except Exception as e:
+            logger.error(e)
+
+
--- api/shell.py
+++ api/shell.py
@@ -1,43 +1,24 @@
-import os
 import re
-import sys
-import subprocess
 import time
-
 from concurrent.futures import ThreadPoolExecutor
 from redisclient import append, hget, hmset, hdel
-from log import logger
 from redisclient import redisClient
 from redislock import RedisLock
-from request import post_form
-sys.path.append("/Users/linrol/work/sourcecode/qiqi/backend/branch-manage")
-sys.path.append("/root/data/sourcecode/qiqi/backend/branch-manage")
-sys.path.append("/data/backend/branch-manage")
-from branch import utils
+from common import Common
 executor = ThreadPoolExecutor()
 
-def chdir_branch():
-    os.chdir("../branch/")
 
-def chdir_data_pre():
-    os.chdir("../dataPre/")
-
-class Shell(utils.ProjectInfo):
+class Shell(Common):
     def __init__(self, user_id, is_test=False, source_branch=None, target_branch=None):
-        chdir_branch()
+        super().__init__()
+        self.chdir_branch()
         self.user_id = user_id
         self.is_test = is_test
-        self.projects = utils.project_path()
         self.source_branch = source_branch
         self.target_branch = target_branch
-        self.project_init_data = self.project = self.projects.get('init-data')
-        self.project_build = self.projects.get('build')
         self.lock = RedisLock(redisClient.get_connection())
         self.lock_value = None
         # self.rest_branch_env()
-
-    def init_branch(self):
-        return self.project_build.getBranch(self.target_branch) is None
 
     # 获取目标分支+当前人是否还存在未合并的mr分支
     def get_open_mr_branch(self, mr_key, branch):
@@ -53,8 +34,8 @@ class Shell(utils.ProjectInfo):
 
     def create_mr(self, mr_key, opened_mr, temp_branch, branch, title, assignee):
         cmd = 'cd {};git push origin {}'.format(self.project_init_data.getPath(), temp_branch)
-        [ret, msg] = subprocess.getstatusoutput(cmd)
-        if ret != 0:
+        ret, msg = self.exec(cmd)
+        if not result:
             return False, msg
         if opened_mr is not None:
             return True, opened_mr.web_url
@@ -70,21 +51,21 @@ class Shell(utils.ProjectInfo):
             # 仅当不存在待合并的分支才创建远程分支
             if opened_mr is None:
                 self.project_init_data.createBranch(self.target_branch, temp_branch)
-            #删除本地分支
+            # 删除本地分支
             self.project_init_data.deleteLocalBranch(temp_branch)
-            #在本地将新分支拉取出来
+            # 在本地将新分支拉取出来
             self.project_init_data.checkout(temp_branch)
-            chdir_data_pre()
-            ret = None
-            msg = None
+            self.chdir_data_pre()
             if data_type == 'new':
                 cmd = 'cd ../dataPre;python3 multi.py {} {} {} {} {}'.format(env, tenant_id, temp_branch, self.user_id, condition_value)
-                [ret, msg] = subprocess.getstatusoutput(cmd)
-            if data_type == 'old':
+                [ret, msg] = self.exec(cmd)
+            elif data_type == 'old':
                 cmd = 'cd ../dataPre;python3 uiconfig.py {} {} {} {} {}'.format(env, tenant_id, temp_branch, self.user_id, condition_value)
-                [ret, msg] = subprocess.getstatusoutput(cmd)
-            chdir_branch()
-            if ret != 0:
+                [ret, msg] = self.exec(cmd)
+            else:
+                [ret, msg] = False, "unknown cmd"
+            self.chdir_branch()
+            if not ret:
                 raise Exception(msg + "\n预制失败，请检查输出日志")
             mr_title = '<数据预置>前端多列表方案预置-{}'.format(self.user_id)
             return self.create_mr(mr_key, opened_mr, temp_branch, self.target_branch, mr_title, mr_user)
@@ -99,45 +80,27 @@ class Shell(utils.ProjectInfo):
     def create_branch(self, fixed_version, project_names):
         try:
             self.lock_value = self.lock.get_lock("lock", 300)
-            init_branch = self.init_branch()
+            clear_build_params = self.get_clear_build_params(self.target_branch)
             is_feature_branch = fixed_version != "None"
             if is_feature_branch:
                 gen_params = "-v {}".format(fixed_version)
             else:
                 gen_params = "-f"
-            [ret, checkout_msg] = self.checkout_branch(self.source_branch)
-            if ret != 0:
-                return False, checkout_msg
+            self.checkout_branch(self.source_branch)
             cmd = 'cd ../branch;python3 createBranch.py {}.stage {}.false {}'.format(self.source_branch, self.target_branch, " ".join(project_names))
-            logger.info("create_branch[{}]".format(cmd))
-            [ret, create_msg] = subprocess.getstatusoutput(cmd)
-            if ret != 0:
-                return False, create_msg
+            [_, created_msg] = self.exec(cmd, True)
             cmd = 'cd ../branch;python3 genVersion.py {} -s {} -t {} -p {}'.format(gen_params, self.source_branch, self.target_branch, ",".join(project_names))
-            logger.info("create_branch[{}]".format(cmd))
-            [ret, gen_version_msg] = subprocess.getstatusoutput(cmd)
-            if ret != 0:
-                return False, gen_version_msg
-            cmd = 'cd ../branch;python3 changeVersion.py {}'.format(self.target_branch)
-            if init_branch:
-                cmd += " true"
-            logger.info("create_branch[{}]".format(cmd))
-            [ret, change_version_msg] = subprocess.getstatusoutput(cmd)
-            if ret != 0:
-                return False, change_version_msg
+            self.exec(cmd, True)
+            cmd = 'cd ../branch;python3 changeVersion.py {} {}'.format(self.target_branch, clear_build_params)
+            [_, version_msg] = self.exec(cmd, True)
             self.commit_and_push(self.target_branch, 'dev' if is_feature_branch else 'hotfix')
-            try:
-                if not self.is_test:
-                    params = {"branch": self.target_branch, "byCaller": "值班助手"}
-                    post_form("http://ops.q7link.com:8000/qqdeploy/projectbuild/", params)
-            except Exception as e:
-                logger.error(e)
-            create_msg = re.compile('WARNNING：.*\n').sub('', create_msg)
-            create_msg = re.compile('工程.*保护成功.*\n').sub('', create_msg)
-            created_value = "{}#{}#{}".format(self.source_branch, self.user_id,
-                                              ",".join(project_names))
-            hmset('q7link-branch-created', {self.target_branch: created_value})
-            return True, (create_msg + "\n" + change_version_msg)
+            self.ops_build(self.target_branch, self.is_test)
+            created_value = "{}#{}#{}".format(self.source_branch, self.user_id, ",".join(project_names))
+            created_mapping = {self.target_branch: created_value}
+            hmset('q7link-branch-created', created_mapping)
+            created_msg = re.compile('WARNNING：.*\n').sub('', created_msg)
+            created_msg = re.compile('工程.*保护成功.*\n').sub('', created_msg)
+            return True, (created_msg + "\n" + version_msg)
         except Exception as err:
             return False, str(err)
         finally:
@@ -147,31 +110,16 @@ class Shell(utils.ProjectInfo):
         try:
             self.lock_value = self.lock.get_lock("lock", 2)
             cmd = 'cd ../branch;python3 checkVersion.py -t compare -b {}'.format(branch_str)
-            [ret, check_version_msg] = subprocess.getstatusoutput(cmd)
-            return ret == 0, check_version_msg
-        except Exception as err:
-            return False, str(err)
-
-    def clear_branch(self, branch_name):
-        try:
-            user_id = self.user_id
-            cmd = 'cd ../branch;python3 checkanddeleted.py {} none'.format(branch_name)
-            [ret, delete_branch_msg] = subprocess.getstatusoutput(cmd)
-            return ret == 0, delete_branch_msg
+            return self.exec(cmd)
         except Exception as err:
             return False, str(err)
 
     def move_branch(self, namespaces):
         try:
             self.lock_value = self.lock.get_lock("lock", 600)
-            [ret, checkout_msg] = self.checkout_branch(self.source_branch)
-            if ret != 0:
-                return False, checkout_msg
+            self.checkout_branch(self.source_branch)
             cmd = 'cd ../branch;python3 backup.py {}.clear {} {},platform,init-data'.format(self.source_branch, self.target_branch, namespaces)
-            logger.info("move_branch[{}]".format(cmd))
-            [ret, backup_msg] = subprocess.getstatusoutput(cmd)
-            if ret != 0:
-                return False, backup_msg
+            [_, backup_msg] = self.exec(cmd, True)
             backup_msg = re.compile('WARNNING：.*\n').sub('', backup_msg)
             backup_msg = re.compile('工程.*创建分支.*\n').sub('', backup_msg)
             backup_msg = re.compile('工程.*删除分支.*\n').sub('', backup_msg)
@@ -184,16 +132,11 @@ class Shell(utils.ProjectInfo):
     def merge_branch(self, clear):
         try:
             self.lock_value = self.lock.get_lock("lock", 600)
-            [ret, checkout_msg] = self.checkout_branch(self.source_branch)
-            if ret != 0:
-                return False, checkout_msg
+            self.checkout_branch(self.source_branch)
             self.protect_branch(self.target_branch, 'release')
             source = self.source_branch + ".clear" if clear else self.source_branch
             cmd = 'cd ../branch;python3 merge.py {} {}'.format(source, self.target_branch)
-            logger.info("merge_branch[{}]".format(cmd))
-            [ret, merge_msg] = subprocess.getstatusoutput(cmd)
-            if ret != 0:
-                return False, merge_msg
+            [_, merge_msg] = self.exec(cmd, True)
             self.protect_branch(self.target_branch, 'none')
             merge_msg = re.compile('WARNNING：.*目标分支.*已存在.*\n').sub('', merge_msg)
             merge_msg = re.compile('工程.*保护成功.*\n').sub('', merge_msg)
@@ -206,37 +149,19 @@ class Shell(utils.ProjectInfo):
     def build_package(self, params, protect, is_build):
         try:
             self.lock_value = self.lock.get_lock("lock", 300)
-            [ret, checkout_msg] = self.checkout_branch(self.target_branch)
-            if ret != 0:
-                return False, checkout_msg
+            self.checkout_branch(self.target_branch)
             cmd = 'cd ../branch;python3 releaseVersion.py {} {} {}'.format(self.source_branch, self.target_branch, params)
-            logger.info("build_package[{}]".format(cmd))
-            [ret, release_version_msg] = subprocess.getstatusoutput(cmd)
-            if ret != 0:
-                return False, release_version_msg
+            [_, release_version_msg] = self.exec(cmd, True)
             cmd = 'cd ../branch;python3 changeVersion.py {}'.format(self.target_branch)
-            logger.info("build_package[{}]".format(cmd))
-            [ret, change_version_msg] = subprocess.getstatusoutput(cmd)
-            if ret != 0:
-                return False, change_version_msg
+            [_, change_version_msg] = self.exec(cmd, True)
             self.commit_and_push(self.target_branch, protect)
-            try:
-                if is_build:
-                    params = {"branch": self.target_branch, "byCaller": "值班助手"}
-                    post_form("http://ops.q7link.com:8000/qqdeploy/projectbuild/", params)
-            except Exception as e:
-                logger.error(e)
+            self.ops_build(self.target_branch, not is_build)
             msg = release_version_msg + change_version_msg
             return True, msg.replace("\n", "").replace("工程", "\n工程")
         except Exception as err:
             return False, str(err)
         finally:
             executor.submit(self.rest_branch_env)
-
-    # 切换所有模块的分支
-    def checkout_branch(self, branch_name):
-        cmd = 'cd ../branch;python3 checkout.py {}'.format(branch_name)
-        return subprocess.getstatusoutput(cmd)
 
     # 重值值班助手环境，切换到master分支，删除本地的target分支
     def rest_branch_env(self):
@@ -250,22 +175,15 @@ class Shell(utils.ProjectInfo):
         if self.lock_value is not None:
             self.lock.del_lock("lock", self.lock_value)
 
-    def protect_branch(self, branch, protect):
-        protect_cmd = "cd ../branch;python3 protectBranch.py {} {}".format(branch, protect)
-        [ret, msg] = subprocess.getstatusoutput(protect_cmd)
-        if ret != 0:
-            logger.info(msg)
-            raise Exception(msg)
-
     def commit_and_push(self, branch, protect):
         self.protect_branch(branch, 'release')
         push_cmd = ''
         for name, project in self.projects.items():
             path = project.getPath()
-            _, project_branch = subprocess.getstatusoutput('cd {};git branch --show-current'.format(path))
-            if project_branch != branch:
+            _, current_branch = self.exec('cd {};git branch --show-current'.format(path), True)
+            if current_branch != branch:
                 continue
-            _, project_commit = subprocess.getstatusoutput("cd {};git status -s".format(path))
+            _, project_commit = self.exec("cd {};git status -s".format(path), True)
             if len(project_commit) < 1:
                 continue
             commit_title = "{}-task-0000-修改版本号--{}({})".format(branch, name, self.user_id)
@@ -273,17 +191,13 @@ class Shell(utils.ProjectInfo):
             push_cmd += ";git push origin {}".format(branch)
         if len(push_cmd) < 1:
             return
-        [ret, msg] = subprocess.getstatusoutput(push_cmd.replace(';', '', 1))
-        if ret != 0:
-            logger.info(msg)
-            raise Exception(msg)
+        self.exec(push_cmd.replace(';', '', 1), True)
         self.protect_branch(branch, protect)
 
 
 if __name__ == "__main__":
-    backup_msg = re.compile('工程.*删除分支.*\n').sub('', "工程【init-data】删除分支【sprint20221002】成功，该分支已合并至分支【stage-global】\n工程【metadata-impl】删除分支【sprint20221002】成功，该分支已合并至分支【stage-global】\n工程【metadata-api】删除分支【sprint20221002】成功，该分支已合并至分支【stage-global】\n工程【mbg-plugins】删除分支【sprint20221002】成功，该分支已合并至分支【stage-global】")
-    shell = Shell('LuoLin', True, 'stage', 'stage-patch20220910')
-    ret, result = shell.create_branch(None, ['arap'])
+    shell = Shell('LuoLin', True, 'stage', 'stage-patch20221023')
+    _, result = shell.create_branch(None, ['app-common'])
     print(result)
 
 
