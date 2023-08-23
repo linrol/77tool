a56f8aac5613044ebd0fbabe8844c9783e41fc4b
linrol <linrolmail@gmail.com>
2023-08-23 14:00:35 +0800

    研发助手-task-0000-提供按工程的pom文件计算权重依赖关系

--- api/dependency.py
+++ api/dependency.py
@@ -6,6 +6,7 @@ XML_NS_INC = "{http://maven.apache.org/POM/4.0.0}"
 class Dependency(Common):
 
     def __init__(self):
+        self.ignore = ['grpc-clients', 'testapp', 'base-common-test', 'idgen-client', 'crypto']
         super().__init__()
 
     def getfile(self, path, level, file_names):
@@ -22,13 +23,34 @@ class Dependency(Common):
                 pom.append(sub)
         return pom
 
+    def get_artifact_list(self, nodes):
+        artifact_list = []
+        for node in nodes:
+            group_node = node.find("{}groupId".format(XML_NS_INC))
+            if group_node is None:
+                continue
+            artifact_node = node.find("{}artifactId".format(XML_NS_INC))
+            if artifact_node is None:
+                continue
+            group = group_node.text
+            artifact = artifact_node.text
+            scope = node.find("{}scope".format(XML_NS_INC))
+            if not group.startswith("com.q7link"):
+                continue
+            if artifact.endswith("-private"):
+                continue
+            if artifact in self.ignore:
+                continue
+            if scope is not None and scope.text == "test":
+                continue
+            artifact_list.append(artifact)
+        return artifact_list
     def cal(self, branch):
-        self.checkout_branch("{}.stage".format(branch))
+        # self.checkout_branch("{}.stage".format(branch))
         et.register_namespace("", XML_NS)
         dependencies = {}
         for project in self.projects.values():
-            ignore = ['grpc-clients', 'testapp', 'base-common-test', 'idgen-client', 'crypto']
-            if project.getName() in ignore:
+            if project.getName() in self.ignore:
                 continue
             path = project.getPath()
             if path is None:
@@ -46,28 +68,19 @@ class Dependency(Common):
                     parent_group = root.find("{}parent/{}groupId".format(XML_NS_INC, XML_NS_INC)).text
                     if parent_group.startswith("com.q7link"):
                         artifact_list.append(parent_module.text)
-                dependency_nodes = root.findall("{}dependencies/{}dependency".format(XML_NS_INC, XML_NS_INC))
-                for node in dependency_nodes:
-                    group = node.find("{}groupId".format(XML_NS_INC)).text
-                    artifact = node.find("{}artifactId".format(XML_NS_INC)).text
-                    scope = node.find("{}scope".format(XML_NS_INC))
-                    if not group.startswith("com.q7link"):
-                        continue
-                    if artifact.endswith("-private"):
-                        continue
-                    if artifact in ignore:
-                        continue
-                    if scope is not None and scope.text == "test":
-                        continue
-                    artifact_list.append(artifact)
-                # print("{}...{}\n".format(module, artifact_list))
+                nodes = root.findall("{}dependencies/{}dependency".format(XML_NS_INC, XML_NS_INC))
+                artifact_list.extend(self.get_artifact_list(nodes))
+                nodes = root.findall("{}build/{}plugins/{}plugin".format(XML_NS_INC, XML_NS_INC, XML_NS_INC))
+                artifact_list.extend(self.get_artifact_list(nodes))
+                if module == "app-common-api":
+                    artifact_list.append("init-data")
                 dependencies[module] = artifact_list
         node = DependencyNode().gen(dependencies, None)
-        weight = 100
+        weight = 500
         while node is not None:
             print("weight:{}, projects:{}".format(weight, node.data))
             node = node.after
-            weight -= 1
+            weight -= 10
 
 
 class Builder(et.TreeBuilder):
@@ -92,9 +105,12 @@ class DependencyNode(object):
         for k, v in data.items():
             if v is None or len(v) == 0:
                 ret.append(k)
+                continue
             before_vs = self.get_before_data(before, list())
-            if set(v) < set(before_vs):
+            if set(v) <= set(before_vs):
                 ret.append(k)
+                continue
+        ret.sort()
         node = DependencyNode(ret, before)
         for k in ret:
             data.pop(k)
