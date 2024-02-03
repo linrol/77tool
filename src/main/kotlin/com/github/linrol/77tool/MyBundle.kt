454ce8277466d649ac7fe15c9e50ef06e3112395
GitHub Action <action@github.com>
2024-02-03 11:46:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/linrol/77tool/MyBundle.kt
+++ src/main/kotlin/com/github/linrol/77tool/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.linrol.77tool
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/linrol/77tool/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/linrol/77tool/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.linrol.77tool.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
