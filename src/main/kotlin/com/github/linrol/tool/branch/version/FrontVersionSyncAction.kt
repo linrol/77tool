5511f57e9076fd2e3e5a176544de3d49b870eb58
linrol <linrolmail@gmail.com>
2024-05-14 12:31:24 +0800

    放开前端版本号同步的功能

--- src/main/kotlin/com/github/linrol/tool/branch/version/FrontVersionSyncAction.kt
+++ src/main/kotlin/com/github/linrol/tool/branch/version/FrontVersionSyncAction.kt
@@ -1,5 +1,6 @@
 package com.github.linrol.tool.branch.version
 
+import com.github.linrol.tool.model.GitCmd
 import com.github.linrol.tool.utils.GitLabUtil
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
@@ -8,12 +9,17 @@ class FrontVersionSyncAction: DumbAwareAction()  {
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
+        val repo = GitLabUtil.getRepository(project, "front-goserver")
+        if (repo == null) {
+            GitCmd.log(project, "请在本地打开front-goserver工程再进行操作")
+            return
+        }
         FrontVersionSyncDialog(project, e).showAndGet()
     }
 
-    override fun update(e: AnActionEvent) {
+    /* override fun update(e: AnActionEvent) {
         val project = e.project ?: return
         val repo = GitLabUtil.getRepository(project, "front-goserver")
         e.presentation.isEnabledAndVisible = repo != null
-    }
+    } */
 }
